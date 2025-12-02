ce003bb26f6c502f4637f95058b8ef5d7bf37a48
sumin8838 <dudtladl0573@naver.com>
2025-12-02 15:25:49 +0900

    09-create-product-details-page

--- components/shared/product/product-card.tsx
+++ components/shared/product/product-card.tsx
@@ -3,6 +3,7 @@ import Link from 'next/link'
 import React from 'react'
 
 import { Card, CardContent, CardHeader } from '@/components/ui/card'
+import ProductPrice from './product-price'
 
 const ProductCard = ({ product }: { product: any }) => {
   return (
@@ -30,7 +31,8 @@ const ProductCard = ({ product }: { product: any }) => {
         <div className="flex-between gap-4">
           <p>{product.rating} stars</p>
           {product.stock > 0 ? (
-            <p className="font-bold">${product.price}</p>
+            // <p className="font-bold">${product.price}</p>
+            <ProductPrice value={Number(product.price)} />
           ) : (
             <p className="text-destructive">Out of Stock</p>
           )}
