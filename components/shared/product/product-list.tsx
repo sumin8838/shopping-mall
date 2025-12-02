2528e119d099d729834ff6374b2a879b9ae7e9ea
sumin8838 <dudtladl0573@naver.com>
2025-12-02 15:07:52 +0900

    07-load-products-from-database

--- components/shared/product/product-list.tsx
+++ components/shared/product/product-list.tsx
@@ -1,14 +1,16 @@
 import React from 'react'
 
 import ProductCard from './product-card'
+import { Product } from '@/types'
 
-const ProductList = ({ data }: { data: any }) => {
+const ProductList = ({ title, data }: { title: string; data: Product[] }) => {
   return (
-    <>
+    <section className="my-10">
+      <h2 className="h2-bold mb-4">{title}</h2>
       {data.length > 0 ? (
         <div>
           <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-            {data.map((product: any) => (
+            {data.map((product: Product) => (
               <ProductCard key={product.slug} product={product} />
             ))}
           </div>
@@ -18,7 +20,7 @@ const ProductList = ({ data }: { data: any }) => {
           <p>No product found</p>
         </div>
       )}
-    </>
+    </section>
   )
 }
 
